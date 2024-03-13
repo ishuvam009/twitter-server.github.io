e6f6ae052b2334af8d786537b9e3c75cb10bd043
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-03-14 02:23:43 +0530

    Initial Commit

---
+++ src/app/indexa.ts
@@ -0,0 +1,9 @@
+import express from 'express'; 
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+
+async function initServer() {
+    const app = express();
+
+    app.use('/graphql',)
+}
--- src/index.ts
+++
@@ -1,1 +0,0 @@
-console.log("Hey my name is Shuvam.");
