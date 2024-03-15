637d0edbc91e0c5c7c83657926789a838ad48cfb
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-03-16 00:12:04 +0530

    Initial Commit

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,13 +1,24 @@
 import express from 'express'; 
+import bodyParser from 'body-parser';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 
 export async function initServer() {
     const app = express();
 
+    app.use(bodyParser.json());
+
     const graphqlServer = new ApolloServer({
-        typeDefs: "",
-        resolvers: {},
+        typeDefs: `
+          type Query {
+              sayHello: String
+          }
+        `,
+        resolvers: {
+          Query: {
+            sayHello: () => `Hello from Qraph Server.`
+          },
+        }, 
       });
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-import { initServer } from "./app/indexa";
+import { initServer } from "./app";
+
+async function init() {
+    const app = await initServer();
+
+    app.listen(8000, () => console.log(`Server is running at PORT 8000`))
+}
+
+init();
